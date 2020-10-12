c1b4249197709bfc54af27b031cd181535a91779
rsuazo <rsuazo2@gmail.com>
2020-10-12 14:21:52 -0700

    saving progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,43 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @passengers = params[:passengers]
 
+    @passengers.to_i.times { @booking.passengers.build }
+
+  end
+
+  def create
+    @booking = Booking.create(flight_id: params[:flight_id])
+
+    test = Booking.create(flight_id: 6)
+
+    Passenger.create(name: "BMW", email: "piper@BMW.com")
+    
+    # Passenger.create(name: params[:booking][:passenger][0][:name], email: params[:booking][:passenger][0][:email])
+  
+    # @booking.passengers << 
+    
+    # @booking.passengers 
+
+
+    # Booking.create(flight_id: 1)
+
+    # Passenger.create(name: "Robert", email: "rsuazo@gmail.com")
+    # Passenger.create(name: "Tiffany", email: "tsuazo@gmail.com")
+    # Passenger.create(name: "Patricia", email: "pringhofer@gmail.com")
+    # Passenger.create(name: "Abigail", email: "aringhofer@gmail.com")
+
+    # Booking.find(1).passengers << Passenger.find(1)
+    # Booking.find(1).passengers << Passenger.find(2)
+    # Booking.find(2).passengers << Passenger.find(3)
+    # Booking.find(2).passengers << Passenger.find(4)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:id, :flight_id, :passenger => [:id, :name, :email], :passenger_attributes => [:id, :name, :email])
+  end
 end
