2c46ca2162fb566bb4944130323dc2bd4e319b4c
rsuazo <rsuazo2@gmail.com>
2020-12-16 20:50:05 -0800

    update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -30,16 +30,15 @@ class BookingsController < ApplicationController
       if @passenger.save
         @booking.passengers << @passenger
 
-
-        
-        from = Email.new(email: 'test@example.com')
-        to = Email.new(email: 'test@example.com')
-        subject = 'Sending with SendGrid is Fun'
-        content = Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-        mail = Mail.new(from, subject, to, content)
+        from = SendGrid::Email.new(email: 'r69er87@gmail.com')
+        to = SendGrid::Email.new(email: 'rsuazo2@gmail.com')
+        subject = 'Sending with Twilio SendGrid is fun...'
+        content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere')
+        mail = SendGrid::Mail.new(from, subject, to, content)
         
         sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
         response = sg.client.mail._('send').post(request_body: mail.to_json)
+        
         puts response.status_code
         puts response.body
         puts response.headers
