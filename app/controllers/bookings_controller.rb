a3ae9c9d5442a837f4c232f51b2b72b7ea69ef01
rsuazo <rsuazo2@gmail.com>
2020-12-16 19:56:13 -0800

    update sendgrid

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -31,18 +31,18 @@ class BookingsController < ApplicationController
       if @passenger.save
         @booking.passengers << @passenger
 
-        from = Email.new(email: 'test@example.com')
-        subject = 'Hello World from the SendGrid Ruby Library!'
-        to = Email.new(email: 'test@example.com')
-        content = Content.new(type: 'text/plain', value: 'Hello, Email!')
-        mail = Mail.new(from, subject, to, content)
+        # from = Email.new(email: 'test@example.com')
+        # subject = 'Hello World from the SendGrid Ruby Library!'
+        # to = Email.new(email: 'test@example.com')
+        # content = Content.new(type: 'text/plain', value: 'Hello, Email!')
+        # mail = Mail.new(from, subject, to, content)
 
-        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-        response = sg.client.mail._('send').post(request_body: mail.to_json)
-        puts response.status_code
-        puts response.body
-        puts response.headers
-        # PassengerMailer.with(user: @passenger).welcome_email.deliver_now
+        # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        # response = sg.client.mail._('send').post(request_body: mail.to_json)
+        # puts response.status_code
+        # puts response.body
+        # puts response.headers
+        PassengerMailer.with(user: @passenger).welcome_email.deliver_now
       end
       
       
