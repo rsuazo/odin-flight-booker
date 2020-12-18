1562fc33052c1024315bb17ce99af9611d31adbe
rsuazo <rsuazo2@gmail.com>
2020-12-17 19:20:26 -0800

    update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -29,19 +29,6 @@ class BookingsController < ApplicationController
 
       if @passenger.save
         @booking.passengers << @passenger
-
-        from = SendGrid::Email.new(email: 'r69er87@gmail.com')
-        to = SendGrid::Email.new(email: @passenger.email)
-        subject = 'Reservation Confirmation'
-        content = SendGrid::Content.new(type: 'text/plain', value: 'Thank you for choosing to stay with us. Wherever your travels take you, our friendly staff will be there every step of the way to ensure your experience is as enjoyable and comfortable as possible. On behalf of our entire team, thank you for allowing us to serve you.')
-        mail = SendGrid::Mail.new(from, subject, to, content)
-        
-        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-        response = sg.client.mail._('send').post(request_body: mail.to_json)
-        
-        puts response.status_code
-        puts response.body
-        puts response.headers
         # PassengerMailer.with(user: @passenger).welcome_email.deliver_now
       end
       
